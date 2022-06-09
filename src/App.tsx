c40b3dfe48a0d3add3d338414106ac5c93647180
pyuung <kyuungah@gmail.com>
2022-06-09 21:46:06 +0900

    feat: home, auth 페이지 기본 라우팅 처리

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,18 @@
+import { lazy } from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+const Home = lazy(() => import('./views/home/Home'))
+const Login = lazy(() => import('./views/login/Login'))
+const SignUp = lazy(() => import('./views/signup/SignUp'))
+
 export default function App() {
-  return <div>App</div>
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </div>
+  )
 }
