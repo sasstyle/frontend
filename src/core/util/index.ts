7e2d411856b8736e89688ddd568ef651c9a0841c
pyuung <kyuungah@gmail.com>
2022-07-06 01:24:06 +0900

    fix: :sparkles: current nav 로직 수정

--- src/core/util/index.ts
+++ src/core/util/index.ts
@@ -1,8 +1,14 @@
 export const getCurrentNav = () => {
-  const location = window.location.pathname.slice(1)
-  const sliceIdx = location.indexOf('/')
-  if (sliceIdx < 0) return location ? location : 'home'
-  else return location.slice(0, sliceIdx)
+  const location = window.location.pathname
+  if (location.includes('store')) return 'store'
+  else if (location.includes('brand')) return 'brand'
+  else if (location.includes('like')) return 'like'
+  else if (location.includes('user')) return 'user'
+  else return 'home'
+
+  // const sliceIdx = location.indexOf('/')
+  // if (sliceIdx < 0) return location ? location : 'home'
+  // else return location.slice(0, sliceIdx)
 }
 
 export const checkReg = (value: any, regExpress: RegExp) => {
